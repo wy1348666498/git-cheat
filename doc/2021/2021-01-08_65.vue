76d239e8ad5a4dc53155fac9f2ecc6581dea0384
Wei Yang <2116466260@qq.com>
2021-01-08 18:26:40 +0800

    feat: 提交vue文件

---
+++ doc/2021/2021-01-08_65.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>浪荡子死于忠贞，阴谋家死于忠诚，偷窃者死于奉献，自私者死于牺牲，向阳花死于黎明之前，野草死于无人问津的风，BE美学有着最残忍也最热烈的爱。--互联网--D5xun</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
