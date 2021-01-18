e4828df41e3ed1f849291050b8a3f2403a917585
Wei Yang <2116466260@qq.com>
2021-01-18 23:13:23 +0800

    feat: 提交vue文件

---
+++ doc/2021/2021-01-18_484.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Sow an act, and you reap a habit. Sow a habit, and you reap a character. Sow a character, and you reap a destiny.--Charles Reade--alterem</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
