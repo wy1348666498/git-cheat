033b38184b77f7667663f997074bf8a9b783ada5
Wei Yang <2116466260@qq.com>
2021-01-21 13:49:19 +0800

    feat: 提交vue文件

---
+++ doc/2021/2021-01-21_916.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>“一个真正想死的人 不会再计较人们说什么 一个拿死说来说去的人 以我的经验来看 并不是真的想死 而是……” “而是什么？” “而是还在……还在渴望爱”--务虚笔记--妖酆</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
