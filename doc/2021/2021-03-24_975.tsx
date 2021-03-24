60757900f0d2d12f0e2a1e9cc723edb620f7785b
Wei Yang <2116466260@qq.com>
2021-03-24 16:33:09 +0800

    feat: 提交tsx文件

---
+++ doc/2021/2021-03-24_975.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>'Cause rainbow won't light up the sky unless you let it rain.--My Little Pony: Friendship is Magic--flowphyl</div>
+}
+
+export default NotFound
