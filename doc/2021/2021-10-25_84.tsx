4331bd639bb2a0b0fbe13925f76a60356ee3da13
Wei Yang <2116466260@qq.com>
2021-10-25 16:33:59 +0800

    feat: 提交tsx文件

---
+++ doc/2021/2021-10-25_84.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>I met the girl under full-bloomed cherry blossoms, and my fate has begun to change.--四月是你的谎言--AThousandMoon</div>
+}
+
+export default NotFound
