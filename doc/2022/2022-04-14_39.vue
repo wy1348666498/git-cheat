d6d4f7133ab99adfa70c006da38fcc5af76b9ed8
Wei Yang <2116466260@qq.com>
2022-04-14 13:56:06 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-04-14_39.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我也有过，只要能用下手姿势把球都接起来，就足够了的想法，而且也自认为确实能够做到这一点，但古人说得好，学无止境啊；明知道自己可以更进一步还不去努力进取的话，不就太无聊了吗？--排球少年--席霖</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
