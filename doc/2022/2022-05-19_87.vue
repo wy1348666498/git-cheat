dad025104915e00beb0fcf3eccff5108ba098a78
Wei Yang <2116466260@qq.com>
2022-05-19 15:54:05 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-05-19_87.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>人各有命，上天注定。有人天生为王，有人落草为寇。脚下的路，如果不是自己的选择，那旅程的终点在哪儿，也没人知道，你会走到哪儿，会碰到谁，都不一定。--镇魂街--龙飞</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
