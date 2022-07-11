ec830128619490a38fcf93bd7d938d87103d23b0
Wei Yang <2116466260@qq.com>
2022-07-12 07:08:10 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-07-12_598.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我经常有那种感觉，如果这个事情来了，你却没有勇敢地去解决掉，它一定会再来。生活真是这样，它会一次次地让你去做这个功课直到你学会为止。--像我这样笨拙地生活--Yousazoe</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
