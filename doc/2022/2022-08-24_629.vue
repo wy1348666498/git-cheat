755ae5bcc0eea8cb438160f1ab1533cc1d87edb7
Wei Yang <2116466260@qq.com>
2022-08-24 22:20:45 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-08-24_629.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>不会停的雨的确存在，但是撑起伞来就行了，接下来抬起头前进，这样一来，偶尔会出现下着太阳雨的日子，经过阳光的反射闪闪发亮的雨滴，一定非常美丽。--秋之回忆Memories Off--青熊兽真烦人</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
