b979204691555972304388e21f45e5b62404e6d3
Wei Yang <2116466260@qq.com>
2022-09-03 18:00:26 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-09-03_352.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>We've learned that friendship isn't always easy, but there's no doubt it's worth fighting for!--My Little Pony: Friendship is Magic--VKorpela</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
