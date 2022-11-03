3c94b44fb545b330457f21b900378cd7c34cf9f5
Wei Yang <2116466260@qq.com>
2022-11-04 02:23:50 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-11-04_191.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>女人是一架钢琴，遇到一位名家来弹，奏出来的是一支名曲;如果是普通人来弹，也许会奏出一支流行曲;要是碰上了不会弹琴的人，恐怕就不成歌了。--火红的五月--a632079</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
