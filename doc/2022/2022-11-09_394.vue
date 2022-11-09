3a30a062eff16f16319016ddac4c65537f2f6271
Wei Yang <2116466260@qq.com>
2022-11-09 15:23:16 +0800

    feat: 提交vue文件

---
+++ doc/2022/2022-11-09_394.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>无论走到哪里，都应该记住，过去都是假的，回忆是一条没有尽头的路，一切以往的春天都不复存在，就连那最坚韧而又狂乱的爱情归根结底也不过是一种转瞬即逝的现实。--百年孤独--Kwokzc</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
