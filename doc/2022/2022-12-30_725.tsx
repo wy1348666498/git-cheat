0b66b6f96a04e06c2e2ad6d95a791a8267f39011
Wei Yang <2116466260@qq.com>
2022-12-30 16:32:33 +0800

    feat: 提交tsx文件

---
+++ doc/2022/2022-12-30_725.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>What, so everyone's supposed to sleep every single night now? You realize that nighttime makes up half of all time?--Rick--zP1nG</div>
+}
+
+export default NotFound
