a5be05e76c8f18f8580d049802f2a2a44a951f62
Wei Yang <2116466260@qq.com>
2023-01-26 04:53:56 +0800

    feat: 提交tsx文件

---
+++ doc/2023/2023-01-26_407.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>I'm bad, and that's good. I will never be good, and that's not bad. There's no one I'd rather be than me.--无敌破坏王--Clementine2019</div>
+}
+
+export default NotFound
