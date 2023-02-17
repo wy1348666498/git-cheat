4ea58ee3953b747a6f8087d94bd8487accc7bb76
Wei Yang <2116466260@qq.com>
2023-02-18 06:36:56 +0800

    feat: 提交vue文件

---
+++ doc/2023/2023-02-18_213.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>终有一天你会跨过静谧无声的洗墨江，离开群山环抱的旧桃源，来到无边阴霾的夜空之下。你要记得，命运悬在刀尖上，而刀尖须得永远向前。愿你在冷铁卷刃前，得以窥见天光。--有匪--一顿吃三斤</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
