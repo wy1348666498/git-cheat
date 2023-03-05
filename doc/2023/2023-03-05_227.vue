2f9171935264d387258340efcf10cafb871d810d
Wei Yang <2116466260@qq.com>
2023-03-05 16:25:17 +0800

    feat: 提交vue文件

---
+++ doc/2023/2023-03-05_227.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>要记住伟大的探知可逆定律:如果你能看到一个低墒世界,那个低嫡世界迟早也能看到你,只是时间问题。所以,什么事情都等别人做是危险的。--三体III--hitokoto</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
