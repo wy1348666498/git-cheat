49582153257f41b1922b638877a325c01087bd7c
Wei Yang <2116466260@qq.com>
2023-03-29 07:52:17 +0800

    feat: 提交vue文件

---
+++ doc/2023/2023-03-29_789.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我们也许再也不会见面了吧，因此我绝对不会忘了你的声音，你的眼眸，与你一起看到过的所有的景色，以及，与你一起度过的那个耀眼的夏天。--草莓100%--hitokoto</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
