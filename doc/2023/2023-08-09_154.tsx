4913569e872d3eda474921b122de892dbc2cc936
Wei Yang <2116466260@qq.com>
2023-08-09 07:30:41 +0800

    feat: 提交tsx文件

---
+++ doc/2023/2023-08-09_154.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>The world is so immense that there must be a you. The world is so immense that I might never run into you.--原创--hitokoto</div>
+}
+
+export default NotFound
