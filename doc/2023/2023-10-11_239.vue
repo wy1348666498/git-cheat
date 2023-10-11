224a1088436d1feabb72fb4ab46d94af44ee5a79
Wei Yang <2116466260@qq.com>
2023-10-11 20:27:22 +0800

    feat: 提交vue文件

---
+++ doc/2023/2023-10-11_239.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>人类文明自身缺陷产生的异化力量、对更高等文明的向往和崇拜、让子孙在终极战争后幸存的强烈欲望，这三股强大的动力推动地球三体运动迅速发展，当它被察觉时，已成燎原之势。--三体I: 地球往事--DreamOne</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
