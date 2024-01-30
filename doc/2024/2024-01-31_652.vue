edb825f0b982d1486e54a0e975370802281c7b12
Wei Yang <2116466260@qq.com>
2024-01-31 01:57:57 +0800

    feat: 提交vue文件

---
+++ doc/2024/2024-01-31_652.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>你就这样任性的走了进来，然后又温柔的走开，留下了温度和遗憾。我做云 ，你做月亮 ，我用两只手遮盖你，我们的屋顶就是青碧的天空出处。--新月集--yudong_820</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
