0c34ae341aba9b3fc0fc3aac4247dbf0c4c72cf4
Wei Yang <2116466260@qq.com>
2024-02-01 06:57:06 +0800

    feat: 提交tsx文件

---
+++ doc/2024/2024-02-01_533.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>We've learned that friendship isn't always easy, but there's no doubt it's worth fighting for!--My Little Pony: Friendship is Magic--VKorpela</div>
+}
+
+export default NotFound
