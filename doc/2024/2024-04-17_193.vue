a7ea197bc7a96c153fb95f53ad0906abecf973b1
Wei Yang <2116466260@qq.com>
2024-04-17 04:08:35 +0800

    feat: 提交vue文件

---
+++ doc/2024/2024-04-17_193.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>人的能力是有极限的。我从短暂的人生当中学到一件事……越是玩弄计谋,就越会发现人类的能力是有极限的……除非超越人类。--JOJO的奇妙冒险--DimensionTripper</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
