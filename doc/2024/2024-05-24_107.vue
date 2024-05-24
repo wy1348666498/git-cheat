918bd9f5a11f34e4df5f5c5cb30e5ce3573e2f8f
Wei Yang <2116466260@qq.com>
2024-05-24 11:10:01 +0800

    feat: 提交vue文件

---
+++ doc/2024/2024-05-24_107.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我们只是历史的过客，目标渺茫，无地自容。我们没有世界大战可以经历，也没有经济大萧条可以恐慌。我们的战争充其量不过是内心之战，我们最大的恐慌就是自己的生活。--搏击俱乐部--Yolly</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
