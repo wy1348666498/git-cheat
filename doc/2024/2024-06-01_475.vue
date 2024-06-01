f045b59238cf29b6613034b33f2cfbb222c389df
Wei Yang <2116466260@qq.com>
2024-06-01 15:45:03 +0800

    feat: 提交vue文件

---
+++ doc/2024/2024-06-01_475.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>We're here to put a dent in the universe. Otherwise why else even be here? 活着就是为了改变世界，难道还有其他原因吗？--乔布斯--海心月</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
